4dc50a2ca644fa310c274db9003cdca396fcce89
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-11-17 18:23:27 +0530

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { IoMenuSharp } from "react-icons/io5";
 function page() {
   return (
     <div className="main w-full h-screen">
-      <div className="navbar max-w-screen-xl bg-blue-500 mx-auto px-6 py-3 flex justify-between items-center">
+      <div className="navbar max-w-screen-xl bg-blue-300 mx-auto px-6 py-3 flex justify-between items-center">
         <h3 className="font-bold text-md">Stake</h3>
         <div className="links gap-4 hidden md:flex">
           <a className="text-md  font-semibold" href="">
@@ -22,6 +22,11 @@ function page() {
         </div>
         <div className="menu block md:hidden">
           <IoMenuSharp />
+        </div>
+      </div>
+      <div className="info max-w-screen-xl mx-auto w-full h-full flex flex-col items-center py-20">
+        <div className="image w-32 h-32 bg-sky-300 rounded-full overflow-hidden">
+          <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
       </div>
     </div>
